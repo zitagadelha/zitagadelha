352d3758a7e56f952817dd006c1cf3cfd6bc5abe
zitagadelha <zrg.onlineads@gmail.com>
2026-07-06 15:48:53 -0300

    Add files via upload

--- js/main-lp.js
+++ js/main-lp.js
@@ -3,7 +3,9 @@
     "https://wa.me/5585988122222?text=Ol%C3%A1.+Vim+do+site+e+gostaria+de+marcar+uma+sess%C3%A3o+particular+com+a+@zitapsi%E2%80%A6.";
 
   document.querySelectorAll("[data-wa]").forEach((el) => {
-    el.setAttribute("href", WA_URL);
+    if (!el.getAttribute("href")) {
+      el.setAttribute("href", WA_URL);
+    }
     el.setAttribute("target", "_blank");
     el.setAttribute("rel", "noopener noreferrer");
     el.addEventListener("click", () => {
